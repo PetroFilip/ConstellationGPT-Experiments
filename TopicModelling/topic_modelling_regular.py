77d52e424dc0f82451318908d27c90a896e2968a
Filip <FILIPP03@pfizer.com>
2023-05-30 15:07:53 -0400

    Updated gitignore

--- TopicModelling/topic_modelling_regular.py
+++ TopicModelling/topic_modelling_regular.py
@@ -6,9 +6,34 @@ from pull_graph_data_constellation_2023 import (
     reconstruct_document,
 )
 
+# create graphdb session
+session = driver.session()
 
 topic_model = BERTopic(embedding_model="emilyalsentzer/Bio_ClinicalBERT")
 # "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
 # "emilyalsentzer/Bio_ClinicalBERT"
 
 # topics, probs = topic_model.fit_transform(docs)
+
+# query graph for document data
+study_ids = get_all_study_names(session)[:3]
+
+docs = []  # holds reconstructed protocols for each study has strings
+for study_id in study_ids:
+    # gather document chunks from graph
+    doc_chunks = grab_all_doc_content_ordered(study_id, session)
+    docs.extend(doc_chunks)
+
+
+topics, probs = topic_model.fit_transform(docs)
+
+df = topic_model.get_topic_info()
+print(df)
+
+# Write DataFrame to a CSV file
+df.to_csv("topics.csv", index=False)
+
+
+# close graphdb session at end
+session.close()
+driver.close()
